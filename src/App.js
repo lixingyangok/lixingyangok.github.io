c1ddac12909dd8c5447e227d132b51ece396f05c
lixingyang <176840078@qq.com>
2020-06-20 07:59:45 +0800

    删除旧文件，新增新文件

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
+/*
+ * @Author: 李星阳
+ * @LastEditors: 李星阳
+ * @Description: 
+ */ 
 import React, {Suspense} from 'react';
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import Navigation from './common/components/navigation.jsx';
@@ -11,10 +16,7 @@ function App() {
       <Switch>
         <Redirect exact from="/" to="/index" ></Redirect>
         <Route path="/index" component={React.lazy(() => import('./pages/index/index.jsx'))} />
-        <Route path="/english" component={React.lazy(() => import('./pages/english/english.jsx'))} />
-        <Route path="/english-2" component={React.lazy(() => import('./pages/english-2/english.jsx'))} />
-        <Route path="/my-tool" component={React.lazy(() => import('./pages/my-tool/my-tool.jsx'))} />
-        <Route path="/my-tool-2" component={React.lazy(() => import('./pages/my-tool-2/my-tool.jsx'))} />
+        <Route path="/my-tool-3" component={React.lazy(() => import('./pages/my-tool-3/my-tool.jsx'))} />
         <Route path="/about" component={React.lazy(() => import('./pages/about/about.jsx'))} />
       </Switch>
     </Suspense>
